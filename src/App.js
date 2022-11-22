054017c5c6d1ee41984ff535af62375ff230ac63
EKhoroshko <xapoxa14@gmail.com>
2022-11-22 02:20:52 +0200

    Contact

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Base from './routers/Base';
 import Home from './routers/Home';
 import Equipment from './routers/Equipment';
+import Contact from './routers/Contact';
 import './style/_App.scss';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Base />}>
           <Route index element={<Home />} />
           <Route path="equipment" element={<Equipment />} />
+          <Route path="contact" element={<Contact />} />
         </Route>
       </Routes>
     </>
